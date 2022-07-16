09f17a7e5a094a2439692582da3c7b305c182bef
MarlonC-A <marlon62009@gmail.com>
2022-07-15 23:03:59 -0500

    Crear Flappy Bird

---
+++ 04_FlappyBird/Pajaro.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var gravedad = 10;
+export var fuerzaSalto = 300;
+export var vTerminal = 200;
+
+var salto = false;
+var velocidad = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	salto = int(Input.is_action_just_pressed("saltar"));
+	var nuevaVelocidad = velocidad + gravedad - fuerzaSalto*salto;
+	velocidad = clamp(nuevaVelocidad, vTerminal*-1, vTerminal);
+	move_and_slide(Vector2(0,velocidad), Vector2(0,1));
+	set_rotation_degrees(velocidad*60/200);
