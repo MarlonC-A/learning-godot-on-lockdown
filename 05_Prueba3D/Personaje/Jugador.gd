0e6f4e1a916d0cae85271e93d9a96942950eb385
MarlonC-A <marlon62009@gmail.com>
2022-07-19 15:52:25 -0500

    Agregar colisiones, salto y giro con quaterniones

--- 05_Prueba3D/Personaje/Jugador.gd
+++ 05_Prueba3D/Personaje/Jugador.gd
@@ -7,7 +7,7 @@ export var gravedad: float = 50;
 var _velocidad := Vector3.ZERO;
 var _vectorSnap := Vector3.DOWN;
 
-onready var _brazoResorte: SpringArm = $SpringArm;
+onready var _brazoResorte: SpringArm = $BrazoResorte;
 onready var _modelo: Spatial = $ModeloArticulado; 
 
 func _ready():
@@ -21,8 +21,25 @@ func _physics_process(delta):
 	
 	_velocidad.x = direccionMov.x * rapidez;
 	_velocidad.z = direccionMov.z * rapidez;
+	_velocidad.y -= gravedad * delta;
 	
+	var aterrizaje := is_on_floor() and _vectorSnap == Vector3.ZERO;
+	var inicioSalto := is_on_floor() and Input.is_action_just_pressed("salto");
+	
+	if inicioSalto:
+		_velocidad.y = fuerza_salto;
+		_vectorSnap = Vector3.ZERO;
+		
+	if aterrizaje:
+		_vectorSnap = Vector3.DOWN;
+		
 	_velocidad = move_and_slide_with_snap(_velocidad,_vectorSnap,Vector3.UP,true);
 	
+	if _velocidad.length() > 0.2:
+		var anguloViejo = Quat(transform.basis);
+		var anguloNuevo = Quat(Vector3(0,1,0),Vector2(direccionMov.z,direccionMov.x).angle());
+		var anguloMedio = anguloViejo.slerp(anguloNuevo,0.5);
+		transform.basis = Basis(anguloMedio);
+		
 func _process(delta):
-	pass
+	_brazoResorte.translation = translation;
