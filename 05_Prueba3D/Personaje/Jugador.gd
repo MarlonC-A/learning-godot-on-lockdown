bdeecbecfb6f93191d6eca4cf75545e7ae6e9e08
MarlonC-A <marlon62009@gmail.com>
2022-07-19 00:55:30 -0500

    Agregar movimiento en 3D

--- 05_Prueba3D/Personaje/Jugador.gd
+++ 05_Prueba3D/Personaje/Jugador.gd
@@ -1,16 +1,28 @@
 extends KinematicBody
 
+export var rapidez: float = 7;
+export var fuerza_salto: float = 20;
+export var gravedad: float = 50;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var _velocidad := Vector3.ZERO;
+var _vectorSnap := Vector3.DOWN;
 
+onready var _brazoResorte: SpringArm = $SpringArm;
+onready var _modelo: Spatial = $ModeloArticulado; 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var direccionMov := Vector3.ZERO;
+	direccionMov.x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda");
+	direccionMov.z = Input.get_action_strength("atras") - Input.get_action_strength("adelante");
+	direccionMov = direccionMov.rotated(Vector3.UP, _brazoResorte.rotation.y).normalized();
+	
+	_velocidad.x = direccionMov.x * rapidez;
+	_velocidad.z = direccionMov.z * rapidez;
+	
+	_velocidad = move_and_slide_with_snap(_velocidad,_vectorSnap,Vector3.UP,true);
+	
+func _process(delta):
+	pass
